b7ca24740fd137522f297ce1de55c6cd63c96c8c
deliteser112 <74626440+deliteser112@users.noreply.github.com>
2021-07-25 11:56:35 +0900

    a

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ const bcrypt = require('bcryptjs');
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:3000'
-  // origin: 'http://3.68.219.73/'
+  // origin: 'http://localhost:3000'
+  origin: 'http://3.68.219.73/'
 };
 
 app.use(cors(corsOptions));
