6eb029085c9d06be68fae7fd0b1dcc700e7ad61e
deliteser112 <74626440+deliteser112@users.noreply.github.com>
2021-07-26 11:42:09 +0900

    port fixed

--- server/dist/server.dev.js
+++ server/dist/server.dev.js
@@ -77,10 +77,10 @@ require('./app/routes/organization.routes')(app);
 require('./app/routes/general.routes')(app);
 
 require('./app/routes/superadmin.routes')(app); // set port, listen for requests
+// const PORT = process.env.PORT || 8080;
 
 
-var PORT = process.env.PORT || 8080; // const PORT = process.env.PORT || 3001;
-
+var PORT = process.env.PORT || 3001;
 app.listen(PORT, function () {
   console.log("Server is running on port ".concat(PORT, "."));
 });
--- server/server.js
+++ server/server.js
@@ -77,8 +77,8 @@ require('./app/routes/general.routes')(app);
 require('./app/routes/superadmin.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
-// const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
